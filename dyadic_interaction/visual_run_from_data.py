db310944bfde47864b23cad41cdfdbb609f0e2cf
Federico <federico.sangati@gmail.com>
2021-09-15 21:57:45 +0900

    improvments - better plots - video in visual

--- dyadic_interaction/visual_run_from_data.py
+++ dyadic_interaction/visual_run_from_data.py
@@ -10,15 +10,21 @@ if __name__ == "__main__":
     
     parser = get_argparse()
     parser.add_argument('--trial', type=int, default=1, choices=[1,2,3,4], help='Trial index')    
+    parser.add_argument('--video_path', type=str, default=None, help='Output video')    
     args = parser.parse_args()
 
     args_dict = vars(args)
-    trial_index = args_dict['trial'] - 1
+    trial_index = args_dict['trial'] - 1    
     del args_dict['trial']
+
+    video_path = None
+    if 'video_path' in args_dict:
+        video_path = args_dict['video_path']
+        del args_dict['video_path']
 
     evo, sim, data_record = obtain_trial_data(**args_dict)
 
-    vis = Visualization(sim)
+    vis = Visualization(sim, video_path=video_path)
     vis.start_simulation_from_data(trial_index, data_record)
 
 
